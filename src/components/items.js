9ce7636b78e1ebb43f3ec746be3b3dc6c6c2f84b
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-09-02 22:06:44 +0530

    Create items.js

---
+++ src/components/items.js
@@ -0,0 +1,76 @@
+let items =[
+    {
+        id:1,
+        image:"./shop/iphone9.png",
+        title: "Apple Product",
+        rating:"⭐⭐⭐⭐",
+        cost:"45,000",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:2,
+        image:"./shop/IQOO Neo.png",
+        title: "Vivo MObile",
+        rating:"⭐⭐⭐⭐⭐",
+        cost:"29,000",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:3,
+        image:"./shop/Realme Narzo 20A.png",
+        title: "MI",
+        rating:"⭐⭐⭐⭐",
+        cost:"19,000",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:4,
+        image:"./shop/Galaxy-Z-Flip4.png",
+        title: "Popular Phone",
+        rating:"⭐⭐⭐⭐⭐",
+        cost:" 47,000",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:5,
+        image:"./shop/Samsung Universe 9.png",
+        title: "Sale Item",
+        rating:"⭐⭐⭐⭐",
+        cost:" 33,000",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:6,
+        image:"./shop/Apple Airpods.png",
+        title: "Ear Product",
+        rating:"⭐⭐⭐⭐",
+        cost:"$15,000",
+        btn:"View option",
+        text : false
+    },
+    {
+        id:7,
+        image:"./shop/asus-laptop.png",
+        title: "Special Item",
+        rating:"⭐⭐⭐⭐",
+        cost:" 1,23,789",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:8,
+        image:"./shop/Sony PlayStation 4 Pro",
+        title: "Popular Item",
+        rating:"⭐⭐⭐",
+        cost:"88,900",
+        btn:"Add to cart",
+        text : false
+    }
+]
+
+export default items;
